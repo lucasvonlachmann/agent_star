4c0d390780bf9ac6ff515fb212940256c069aeb8
lucas von lachmann <lucasvonlachmann@hotmail.com>
2020-06-22 14:20:23 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#users
+u1 = User.create!(email: "agent@mail.com", password: "123456")
+puts "users created"
+
+# ports
+p1 = Port.create!(name: "RIO DE JANEIRO")
+p2 = Port.create!(name: "SANTOS")
+p3 = Port.create!(name: "ITAJAI")
+puts "ports created"
+
+# vessels
+v1 = Vessel.create!(name: "VESSEL 1")
+v2 = Vessel.create!(name: "VESSEL 2")
+v3 = Vessel.create!(name: "VESSEL 3")
+puts "vessels created"
+
+# carriers
+maersk = Carrier.create!(name: "MAERSK")
+alianca = Carrier.create!(name: "ALIANÇA")
+puts "carriers created"
+
+# kpi types
+wt = KpiType.create!(name: "WAITING TIME", vw_name: "waiting_time")
+ia = KpiType.create!(name: "IDLE TIME ARRIVAL", vw_name: "idle_time_arrival")
+id = KpiType.create!(name: "IDLE TIME DEPARTURE", vw_name: "idle_time_departure")
+puts "kpis types created"
+
+# root causes
+RootCause.create!(kpi_type: wt, name: "MINIMUM SPEED")
+RootCause.create!(kpi_type: wt, name: "MANEUVERS")
+RootCause.create!(kpi_type: wt, name: "BERTH CONGESTION")
+RootCause.create!(kpi_type: ia, name: "TERMINAL")
+RootCause.create!(kpi_type: ia, name: "PRE STOWAGE PLAN")
+RootCause.create!(kpi_type: ia, name: "LINE DECISION")
+RootCause.create!(kpi_type: id, name: "MANEUVERS")
+RootCause.create!(kpi_type: id, name: "REEFER MALFUNCTION")
+RootCause.create!(kpi_type: id, name: "TIDE RESTRICTION")
+puts "root causes created"
+
+# kpis
+Carrier.all.each do |carrier|
+  Port.all.each do |port|
+    KpiType.all.each do |kpi_type|
+      Kpi.create!(carrier: carrier,
+                  port: port,
+                  kpi_type: kpi_type,
+                  target: [60, 80, 100, 120, 140].sample,
+                  max_limit: 0, min_limit: 0)
+    end
+  end
+end
+puts "kpis created"
+
+# scales
+counter = 1
+Carrier.all.each do |carrier|
+  Port.all.each do |port|
+    Vessel.all.each do |vessel|
+      Scale.create!(carrier: carrier,
+                  port: port,
+                  vessel: vessel,
+                  voyage: "VOYAGE - #{counter}",
+                  service: ["SERVICE - 1", "SERVICE - 2", "SERVICE - 3", "SERVICE - 4"].sample)
+      counter += 1
+    end
+  end
+end
+puts "scales created"
